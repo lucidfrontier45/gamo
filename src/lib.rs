8a47beb35f489a1aa63e191bf7e532497ab34412
杜 世橋 Du Shiqiao <lucidfrontier.45@gmail.com>
2024-01-12 09:17:47 +0900

    corrected unittest

--- src/lib.rs
+++ src/lib.rs
@@ -90,7 +90,7 @@ mod tests {
         assert_eq!(r.next(), Some(TimeSlot(5)));
         assert_eq!(r.next(), None);
 
-        let mut r = Gamo::new(TimeSlot(5), TimeSlot(4));
+        let mut r = Gamo::new_inclusive(TimeSlot(5), TimeSlot(4));
         assert_eq!(r.next(), None);
     }
 }
